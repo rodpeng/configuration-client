7493d22d2371a6d04fcb721ec19276c4694e372f
Peng, Rod <Rod.Peng@disney.com>
2018-11-11 13:34:04 +0800

    initial commit

---
+++ src/main/java/com/rodpeng/configuration/client/ConfigClientApplication.java
@@ -0,0 +1,36 @@
+package com.rodpeng.configuration.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+	public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${message:Hello default}")
+    private String message;
+    
+    @Value("${password: default}")
+    private String password;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+    
+    @RequestMapping("/password")
+    String getPassword() {
+        return this.password;
+    }
+}
